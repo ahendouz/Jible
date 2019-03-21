3dc52d2c42004077a772a74cb3ddc4dd7251e298
hicham ahendouz <ahendouz.dev@gmail.com>
2019-03-21 16:49:28 +0100

    Add a location

--- web/src/actions/profileAction.js
+++ web/src/actions/profileAction.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import { ADD_LOCATION } from "./types";
 
 export const editUserProfileAction = profileFields => dispatch => {
   axios
     .post("/api/profile/edit_profile", profileFields)
     .then(res => console.log("🚚🚚🚚🚚", res));
+};
+export const addLocation = address => async dispatch => {
+  axios
+    .post("/api/profile/add_location", {
+      address
+    })
+    .then(res =>
+      dispatch({
+        type: ADD_LOCATION,
+        payload: res.data
+      })
+    );
 };
